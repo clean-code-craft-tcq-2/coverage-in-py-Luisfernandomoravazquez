f43d385d69de2b220b8cc77e9923df06a31b272a
mur1ga <mur1ga@rbcm-gerrit.de.bosch.com>
2022-02-18 18:32:17 -0600

    Functionality done, test still to be implemented

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -4,8 +4,9 @@ import typewise_alert
 
 class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
-    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
-
+    self.assertTrue(typewise_alert.inputFunction(-3,"PASSIVE") == 'TOO_LOW')
+    self.assertTrue(typewise_alert.inputFunction(10,"PASSIVE") == 'NORMAL')
+    self.assertTrue(typewise_alert.inputFunction(90,"PASSIVE") == 'TOO_HIGH')
 
 if __name__ == '__main__':
   unittest.main()
